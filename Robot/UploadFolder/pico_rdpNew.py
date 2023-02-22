02ef71ef18f600793dcd575efe14a0efa584c1b7
JAWB86 <james.jawb86@gmail.com>
2023-02-22 12:21:51 +0000

    from robot

--- Robot/UploadFolder/pico_rdpNew.py
+++ Robot/UploadFolder/pico_rdpNew.py
@@ -215,29 +215,7 @@ class Ultrasound_gain():
         time.sleep(0.01)
         self.spi.write(msg)
         self.cs.value(1)
-
-class Ultrasonic():
-    def __init__(self, trig_Pin, echo_Pin):
-        """Initialize Input(echo) and Output(trig) Pins."""
-        self._trig = Pin(trig_Pin, Pin.OUT)
-        self._echo = Pin(echo_Pin, Pin.IN)
-        self._sound_speed = 340.0  # m/s
-
-    def _pulse(self):
-        """Trigger ultrasonic module with 10us pulse."""
-        self._trig.high()
-        time.sleep_us(10)
-        self._trig.low()
-
-    def get_distance(self):
-        """Measure pulse length and return calculated distance [cm]."""
-        self._pulse()
-        pulse_width_s = time_pulse_us(self._echo, Pin.on) / 10000
-        #print(pulse_width_s)
-        dis = (pulse_width_s / 2.0) * self._sound_speed
-        return dis
-
-
+        
 class Motor():
     def __init__(self, pin_a, pin_b, dir=1):
         self.pin_1 = PWM(Pin(pin_a, Pin.OUT))
@@ -282,7 +260,6 @@ class Motor():
 
     def set_motor_power(self, power):
         self.power = power
-
 
 @asm_pio(sideset_init=PIO.OUT_LOW, out_shiftdir=PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
 def ws2812():
