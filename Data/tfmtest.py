d85100c944d585fd35f12504104cfcbea73c1ba0
JAWB86 <james.jawb86@gmail.com>
2023-03-21 15:58:33 +0000

    Isosurface plot

--- Data/tfmtest.py
+++ Data/tfmtest.py
@@ -11,8 +11,8 @@ from scipy.interpolate import interp1d
 # use the total focusing method to find the responses in 3d space
 
 # create a 3d meshgrid
-x = np.linspace(-0.25, 0.25, 101)
-y = np.linspace(-0.25, 0.25, 101)
+x = np.linspace(-0.25, 0.25, 51)
+y = np.linspace(-0.25, 0.25, 51)
 z = np.linspace(0.01, 0.5, 50)
 X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
 
@@ -29,12 +29,14 @@ X_edges, Y_edges, Z_edges = np.meshgrid(
 # create a PseudoTimeDomain object
 pseudo_signal = ptd.PseudoTimeDomain(15, 20)
 
+pipe_radius = 150 * 1e-3
+
 
 radui = [7.5]
 gain_time = []
 # load data
 for sensor_radius in radui:
-    with open(os.path.join(Path(__file__).resolve().parents[1], os.path.join("Robot", os.path.join("UploadFolder", "scan_data_time_"+str(sensor_radius)+".csv"))), newline='') as f:
+    with open(os.path.join(Path(__file__).resolve().parents[1], os.path.join("Robot", os.path.join("UploadFolder", "scan_data_time_8.1.csv"))), newline='') as f:
         reader = csv.reader(f)
         gain_time_temp = list(reader)
     gain_time_2d = []
@@ -45,7 +47,7 @@ for sensor_radius in radui:
 sensor_angles = np.arange(0, 360, int(360/len(gain_time[0])))-90  # start at 9 o'clock
 
 # use the pseudo time domain object to find the responses using the gain time data
-# important the [1]!!!!!!!!!!
+# important the [0]!!!!!!!!!!
 # find largest value in gain_time[0]
 max_gain_time = max([max(i) for i in gain_time[0]])
 
@@ -81,15 +83,16 @@ print("Loop complete")
 
 # find the abs of the responses
 responses = np.abs(responses)
+responses[responses == 0] = 1e-10
 
 # convert to dB
-responses = 20*np.log10(responses)
+responses = 20*np.log10(responses/np.max(responses))
 
 to_plot_x = int(1*len(x)/2)
 to_plot_y = int(1*len(y)/2)
 to_plot_z = int(1*len(z)/2)
 
-colour_scale = dict(cmin=0, cmax=np.max(responses))
+colour_scale = dict(cmin=-10, cmax=np.max(responses), colorscale="Jet")
 
 X_plot, Y_plot = np.meshgrid(x, y)
 Z_plot = np.ones_like(X_plot)*z[to_plot_z]
@@ -105,10 +108,11 @@ Y_plot, Z_plot = np.meshgrid(y, z)
 X_plot = np.ones_like(Y_plot)*x[to_plot_x]
 x_slice = go.Surface(x=X_plot, y=Y_plot, z=Z_plot,
                      surfacecolor=responses[to_plot_x, :, :].T, **colour_scale)
+
+# plot the slices
 fig1 = go.Figure(data=[x_slice, y_slice, z_slice])
 fig1.update_layout(
-    coloraxis=dict(colorscale='BrBG',
-                   colorbar_thickness=25,
+    coloraxis=dict(colorbar_thickness=25,
                    colorbar_len=0.75,
                    **colour_scale))
 fig1.show()
@@ -189,39 +193,84 @@ fig2.update_layout(
 fig2.show()
 
 
+
+# Plot Pipe Wall
+pipe_theta = np.linspace(0, 2*np.pi, 100)
+pipe_zc = z
+pipe_ang, pipe_zc = np.meshgrid(pipe_theta, pipe_zc)
+pipe_xc = pipe_radius * np.cos(pipe_ang)
+pipe_yc = pipe_radius * np.sin(pipe_ang)
+
+pipe_color = [[0, 'red'],
+             [1, 'red']]
+
+#pio.renderers.default = "browser" # "svg" "browser"
+fig3 = go.Figure(data=go.Isosurface(
+    x = Z.flatten() * 1e2,
+    y = X.flatten() * 1e2,
+    z = Y.flatten() * 1e2,
+    value=responses.flatten(),
+    isomin=-15,
+    isomax=0,
+    caps=dict(x_show=False, y_show=False),
+    colorscale='jet',
+    showscale=False,
+    surface_count=6,
+    opacity=0.3
+    ))
+fig3.add_trace(go.Surface(
+   x = pipe_zc * 1e2,
+   y = pipe_xc * 1e2,
+   z = pipe_yc * 1e2,
+   colorscale = pipe_color,
+   showscale=False,
+   opacity=0.1
+   ))
+camera = dict(eye=dict(x=1.5, y=2.5, z=0.6))
+fig3.update_layout(scene_camera = camera)
+fig3.update_layout(scene_aspectmode='data')
+fig3.update_layout(margin=dict(r=10, b=10, l=10, t=10))
+fig3.update_layout(scene=dict(xaxis_title='Z (cm)', yaxis_title='X (cm)', zaxis_title='Y (cm)'),
+                  font=dict(family="verdana", color="Black", size=18))
+fig3.show()
+# fig.write_html("images/brick1_3d_saft.html")
+
+
+
 # save reponses to a mat file
 sio.savemat('responses.mat', {'responses': responses})
 
+plot_all = False
+if (plot_all == True):
+    # plot x, y and y slices of the responses in 2d using imshow on their own figures
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(responses[to_plot_x, :, :], extent=[
+            z[0], z[-1], y[-1], y[0]], aspect='auto')
+    ax.set_title('x = ' + str(x[to_plot_x]))
+    ax.set_xlabel('z')
+    ax.set_ylabel('y')
 
-# plot x, y and y slices of the responses in 2d using imshow on their own figures
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.imshow(responses[to_plot_x, :, :], extent=[
-          z[0], z[-1], y[-1], y[0]], aspect='auto')
-ax.set_title('x = ' + str(x[to_plot_x]))
-ax.set_xlabel('z')
-ax.set_ylabel('y')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(responses[:, to_plot_y, :], extent=[
+            z[0], z[-1], x[-1], x[0]], aspect='auto')
+    ax.set_title('y = ' + str(y[to_plot_y]))
+    ax.set_xlabel('z')
+    ax.set_ylabel('x')
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.imshow(responses[:, to_plot_y, :], extent=[
-          z[0], z[-1], x[-1], x[0]], aspect='auto')
-ax.set_title('y = ' + str(y[to_plot_y]))
-ax.set_xlabel('z')
-ax.set_ylabel('x')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(responses[:, :, to_plot_z], extent=[
+            y[0], y[-1], x[-1], x[0]], aspect='auto')
+    ax.set_title('z = ' + str(z[to_plot_z]))
+    ax.set_xlabel('y')
+    ax.set_ylabel('x')
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.imshow(responses[:, :, to_plot_z], extent=[
-          y[0], y[-1], x[-1], x[0]], aspect='auto')
-ax.set_title('z = ' + str(z[to_plot_z]))
-ax.set_xlabel('y')
-ax.set_ylabel('x')
-
-# replace 0s with nans
-responses[responses == 0] = np.nan
-# plot the responses in 3d using scatter
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X, Y, Z, c=responses)
-plt.show()
+    # replace 0s with nans
+    responses[responses == 0] = np.nan
+    # plot the responses in 3d using scatter
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(X, Y, Z, c=responses)
+    plt.show()
